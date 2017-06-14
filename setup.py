9c093e1eeeb7231ec8b4ffc4e82d004d83d3d79c
bewestphal <brianewestphal@gmail.com>
2017-06-14 16:42:57 -0700

    add tag

--- setup.py
+++ setup.py
@@ -1,27 +1,41 @@
 from setuptools import setup
 from pip.req import parse_requirements
+from pip.download import PipSession
 
-reqs = install_reqs = parse_requirements('requirements.txt')
+links = []
+requires = []
 
-setup(name='SeleniumAI',
+requirements = parse_requirements('requirements.txt', session=PipSession())
+
+for item in requirements:
+    # we want to handle package names and also repo urls
+    if getattr(item, 'url', None):  # older pip has url
+        links.append(str(item.url))
+    if getattr(item, 'link', None): # newer pip has link
+        links.append(str(item.link))
+    if item.req:
+        requires.append(str(item.req))
+
+setup(name='seleniumai',
       description='An openAI environment that uses Selenium to create web automation agents',
-      version='0.0.2',
+      version='0.0.5',
       url='https://github.com/bewestphal/SeleniumAI',
       author='Brian Westphal',
       author_email='coding@brianwestphal.com',
       license='MIT',
+      download_url='https://github.com/bewestphal/SeleniumAI',
       keywords=[
           'selenium',
           'artificial intelligence',
           'openai',
           'environment'
       ],
-      packages = ['lib'],
+      packages = ['package'],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 2.7',
       ],
-      nstall_requires=reqs
-)
+      dependency_links=links,
+      install_requires=requires)
